f1d3d136247b6d7a0a97dfd1f1363809867f06cf
qathom <anthony.me@protonmail.ch>
2020-04-01 22:50:33 +0200

    Added progressbar

--- app/src/main/java/com/test/ar/MainActivity.java
+++ app/src/main/java/com/test/ar/MainActivity.java
@@ -3,6 +3,7 @@ package com.test.ar;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -32,9 +34,6 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.util.ArrayList;
 
-/**
- * This activity uses the Sceneform UX package to make common AR tasks easier.
- */
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
@@ -42,13 +41,13 @@ public class MainActivity extends AppCompatActivity {
     private ArFragment arFragment;
     private ModelReference selectedModel = null;
     private ModelManager modelManager;
-    private NodeManager nm;
+    private NodeManager nodeManager;
     private InfoDialog dialog;
-    private LoadModelTask modelTask;
     private Button buttonSelectModel;
     private ModelAnimator animator;
     private int nextAnimation;
     private ArrayList<String> objectVisited = new ArrayList<>();
+    private ProgressDialog progressDialog;
 
     /**
      * Threshold before showing the modal
@@ -81,25 +80,34 @@ public class MainActivity extends AppCompatActivity {
         buttonSelectModel.setOnClickListener((View view) -> {
             showModelModal();
         });
-        buttonSelectModel.setEnabled(false);
 
         // Delete/reset all objects
         Button buttonReset = (Button) findViewById(R.id.buttonReset);
         buttonReset.setOnClickListener((View view) -> {
-            this.nm.deleteAll();
+            nodeManager.deleteAll();
         });
 
-        // Render 3D models
-        modelTask = new LoadModelTask();
-        modelTask.execute();
+        // Progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Loading model...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
 
         // Hide navigation bar
         hideNavigationBar();
+
+        // Setup AR
+        setupAr();
     }
 
     private void setupAr() {
+        // On tap listener
         arFragment.setOnTapArPlaneListener(this::onPlaneTap);
-        nm = new NodeManager(this, arFragment);
+
+        // On update listener
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdate);
+
+        nodeManager = new NodeManager(this, arFragment);
     }
 
     private void hideNavigationBar() {
@@ -112,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
     }
 
-    private void onPlayAnimation(ModelRenderable model) {
+    private void playAnimation(ModelRenderable model) {
         try {
             if (animator == null || !animator.isRunning()) {
                 AnimationData data = model.getAnimationData(nextAnimation);
@@ -120,7 +128,6 @@ public class MainActivity extends AppCompatActivity {
                 nextAnimation = (nextAnimation + 1) % model.getAnimationDataCount();
                 animator = new ModelAnimator(data, model);
                 animator.start();
-                Toast toast = Toast.makeText(this, data.getName(), Toast.LENGTH_SHORT);
 
                 Log.d(
                         TAG,
@@ -133,32 +140,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Listener for plane tap gestures
+     * @param hitResult
+     * @param plane
+     * @param motionEvent
+     */
     private void onPlaneTap(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
         if (selectedModel == null) {
+            // Open the modal so that the user can select an object
             showModelModal();
-            Toast toast =
-                    Toast.makeText(this, "Select an object to insert in the scene", Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.CENTER, 0, 0);
-            toast.show();
             return;
         }
 
-        // Create the transformable andy and add it to the anchor.
-        nm.add(arFragment, hitResult, selectedModel);
-
-        // Update
-        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdate);
+        // Create the transformable node and add it to the anchor.
+        nodeManager.add(arFragment, hitResult, selectedModel);
     }
 
     private String getContextualText(TransformableNode node) {
         long count = objectVisited.stream().filter(n -> n.equals(node.getName())).count();
         String base = node.getName().toUpperCase();
 
-        if (count > 1) {
-            return base + "\n\n" + "Object visited " + count + " times.";
+        if (count >= 1) {
+            return base + "\n\n" + "Welcome back! You already visited this object " + count + " times.";
         }
 
-        return base + "\n\n" + "Visited for the first time.";
+        return base + "\n\n" + "This is your first visit.";
     }
 
     public void showInfoDialog(TransformableNode node) {
@@ -198,21 +205,33 @@ public class MainActivity extends AppCompatActivity {
         // ArrayList to String[]
         String[] stringArray = modelManager.getModelResources().toArray(new String[0]);
 
-        builder.setItems(stringArray, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int index) {
-                String modelName = stringArray[index];
-                ModelReference model = modelManager.get(modelName);
+        builder.setItems(stringArray, (dialog, index) -> {
+            String modelName = stringArray[index];
 
-                if (model == null) {
-                    Log.e(TAG, "Invalid model for: " + modelName);
-                    return;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    ModelReference model = modelManager.get(modelName);
+
+                    if (model != null) {
+                        selectedModel = model;
+                        showPlaceObjetToast(modelName);
+                        return;
+                    }
+
+                    progressDialog.show();
+
+                    modelManager.buildModel(modelName)
+                            .whenComplete((r, t) -> {
+                                selectedModel = modelManager.get(modelName);
+
+                                progressDialog.dismiss();
+
+                                showPlaceObjetToast(modelName);
+                            });
                 }
-
-                Toast.makeText(getApplicationContext(), "You can now place" + " " + modelName, Toast.LENGTH_LONG).show();
-
-                selectedModel = model;
-            }
+            });
         });
 
         AlertDialog dialog = builder.create();
@@ -220,46 +239,56 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    private void showPlaceObjetToast(String modelName) {
+        Toast.makeText(getApplicationContext(), "You can now place" + " " + modelName, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     *
+     * @param unusedframeTime
+     */
     public void onUpdate(FrameTime unusedframeTime) {
         Frame frame = arFragment.getArSceneView().getArFrame();
         Camera camera = frame.getCamera();
 
-        if (camera.getTrackingState() == TrackingState.TRACKING) {
-            Vector3 cameraPosition = arFragment.getArSceneView().getScene().getCamera().getWorldPosition();
+        if (camera.getTrackingState() != TrackingState.TRACKING) {
+            return;
+        }
 
-            // Get closest object
-            ArrayList<TransformableNode> nodes = nm.getAll();
-            TransformableNode closestNode = null;
+        Vector3 cameraPosition = arFragment.getArSceneView().getScene().getCamera().getWorldPosition();
 
-            float closestDistance = -1;
+        // Get closest object
+        ArrayList<TransformableNode> nodes = nodeManager.getAll();
+        TransformableNode closestNode = null;
 
-            for (TransformableNode node : nodes) {
-                // Use the utility class to get distance between vectors
-                float distance = Utility.getDistanceBetweenVectors(cameraPosition, node.getWorldPosition());
+        float closestDistance = -1;
 
-                if (closestDistance == -1 || distance < closestDistance) {
-                    closestNode = node;
-                    closestDistance = distance;
-                    node.getRenderable();
-                }
+        for (TransformableNode node : nodes) {
+            // Use the utility class to get distance between vectors
+            float distance = Utility.getDistanceBetweenVectors(cameraPosition, node.getWorldPosition());
+
+            if (closestDistance == -1 || distance < closestDistance) {
+                closestNode = node;
+                closestDistance = distance;
+                node.getRenderable();
             }
-            ;
+        }
+        ;
 
-            if (closestNode == null) {
-                return;
-            }
+        if (closestNode == null) {
+            return;
+        }
 
-            // Distance
-            if (closestDistance < thresholdShowModal) {
-                // Focus object
-                closestNode.select();
+        // Distance
+        if (closestDistance < thresholdShowModal) {
+            // Focus object
+            closestNode.select();
 
-                showInfoDialog(closestNode);
+            showInfoDialog(closestNode);
 
-                onPlayAnimation(modelManager.get(closestNode.getName()).getModel());
-            } else {
-                hideInfoDialog();
-            }
+            playAnimation(modelManager.get(closestNode.getName()).getModel());
+        } else {
+            hideInfoDialog();
         }
     }
 
@@ -270,35 +299,6 @@ public class MainActivity extends AppCompatActivity {
         // Remove listener
         if (arFragment != null) {
             arFragment.setOnTapArPlaneListener(null);
-        }
-
-        // Cancel running task(s) to avoid memory leaks
-        if (modelTask != null) {
-            modelTask.cancel(true);
-        }
-    }
-
-    private class LoadModelTask extends AsyncTask<Void, Void, Boolean> {
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // Render models on UI thread
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    modelManager.buildAll();
-                }
-            });
-
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean res) {
-            // Setup fragment once the models are ready
-            setupAr();
-
-            // Enable object selection
-            buttonSelectModel.setEnabled(res);
         }
     }
 
